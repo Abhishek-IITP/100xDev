fd5ec99db49fcb4f3e9fbdf5708a3054cd5588c9
Abhishek Kumar Mohanty <165031056+Abhishek-IITP@users.noreply.github.com>
2026-01-03 13:27:25 +0530

    new commit

---
+++ DSA/Day1/codeforces/add_last_digit.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int num1;
+    int num2;
+    cin>>num1>>num2;
+    
+    int a = num1%10;
+    int b = num2%10;
+    
+    cout<< a + b<<endl;
+    
+}
