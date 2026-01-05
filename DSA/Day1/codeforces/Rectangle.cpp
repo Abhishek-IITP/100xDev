8b7bae4aa4f85b46808e6bbf56bc73c654e4a029
Abhishek Kumar Mohanty <165031056+Abhishek-IITP@users.noreply.github.com>
2026-01-05 13:24:13 +0530

    new commit

---
+++ DSA/Day1/codeforces/Rectangle.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int length;
+    int breadth;
+    
+    cin>>length>>breadth;
+    
+    
+    int Perimeter = 2*(length+breadth);
+    int Area = length *breadth;
+    
+    cout<<"Area = "<<Area<<endl;
+    cout<<"Perimeter = "<<Perimeter<<endl;
+}
