e499a81a4f9990d4dfcbcb3a5380bcacf1dc9c30
Abhishek Kumar Mohanty <165031056+Abhishek-IITP@users.noreply.github.com>
2026-01-08 13:21:29 +0530

    new commit

---
+++ DSA/Day1/codeforces/N.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    int y;
+    cin>>x>>y;
+    
+ cout << "Min = " << min(x, y) << endl;
+cout << "Max = " << max(x, y) << endl;
+
+    
+}
