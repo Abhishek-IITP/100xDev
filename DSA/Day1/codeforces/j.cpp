3e51341cd2a27a266e039d54d21b0cf7d8da7d3e
Abhishek Kumar Mohanty <165031056+Abhishek-IITP@users.noreply.github.com>
2026-01-09 13:21:01 +0530

    new commit

---
+++ DSA/Day1/codeforces/j.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int num1;
+    cin>>num1;
+    
+    if(num1%2==0){
+        cout<<"Even"<<endl;
+    }
+    else cout<<"Odd"<<endl;
+    
+}
