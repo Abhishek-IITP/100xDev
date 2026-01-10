f18d9eb7ec7ee4752ecf7de4ef8104e7b54e7f7a
Abhishek Kumar Mohanty <165031056+Abhishek-IITP@users.noreply.github.com>
2026-01-10 13:20:20 +0530

    new commit

---
+++ DSA/Day1/codeforces/k.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    int F;
+    cin>>N>>F;
+    
+    if(N%F==0){
+        cout<<"Yes"<<endl;
+    }
+    else cout<<"No"<<endl;
+    
+}
