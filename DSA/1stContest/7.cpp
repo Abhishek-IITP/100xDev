db2f2f18abc26af40f9adaf97c40cfb21d94efff
Abhishek Kumar Mohanty <165031056+Abhishek-IITP@users.noreply.github.com>
2026-01-17 13:12:52 +0530

    new commit

---
+++ DSA/1stContest/7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long N;
+    cin>>N;
+    int count =0;
+    if(N == 0) {
+    cout<<1;
+    return 0;    
+    }
+
+    while (N > 0) {
+        if (N % 10 == 0)
+            count++;
+        N /= 10;
+    }
+
+    cout << count;
+    return 0;
+}
