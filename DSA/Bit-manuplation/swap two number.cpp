c99e7dade142b2ead9dd40a9a976e07bf362d78e
Abhishek-IITP <abhishekmohanty7325@gmail.com>
2026-09-14 11:46:05 +0530

    bit manuplation questions

---
+++ DSA/Bit-manuplation/swap two number.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a,b;
+	cin>>a>>b;
+	
+	a = a +b;
+	b= a -b;
+	a= a-b;
+	
+	cout<<"value of a:"<<a<<endl;
+	cout<< "value of b:"<<b<<endl;
+
+    return 0;
+}
+
